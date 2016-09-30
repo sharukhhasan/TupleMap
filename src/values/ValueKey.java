18644d730f113fbb62c9822c887d28fcd2cc116b
Sharukh Hasan <sharukhhasan@hypercryptic.com>
2016-09-30 01:49:08 -0500

    added interface for value key and label

---
+++ src/values/ValueKey.java
@@ -0,0 +1,8 @@
+package values;
+
+/**
+ * Created by Sharukh Hasan on 9/30/16.
+ */
+public interface ValueKey<X> {
+    public X getKey();
+}
---
+++ src/values/ValueLabel.java
@@ -0,0 +1,8 @@
+package values;
+
+/**
+ * Created by Sharukh Hasan on 9/30/16.
+ */
+public interface ValueLabel<X> {
+    public X getLabel();
+}
